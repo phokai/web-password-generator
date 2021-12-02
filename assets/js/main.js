a65603ac6665d1b2d3f9ff9466294aa2fa855b49
AtaKan <43812088+phokai@users.noreply.github.com>
2021-12-03 00:29:37 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"$% &/()=?@~`\\.';:+=^*_-0123456789";
+
+let output = document.getElementById("output");
+
+function randomValue(value) {
+    return Math.floor(Math.random() * value);
+}
+
+function genPassword() {
+    let length = document.getElementById("length").value;
+    document.getElementById("length-val").textContent = length;
+    let str = "";
+
+    for(let i = 0; i < length; i++) {
+        let random = randomValue(characters.length);
+        str += characters.charAt(random);
+    }
+    output.value = str;
+}
+function copyPassword() {
+    output.select();
+    document.execCommand("copy");
+    alert("Parola kopyalandı.");
+}
+genPassword();
